270b0f5c8a81cd840433c58ec53c352b1317fb23
Noah Franck <Noah@Noahs-iMac-3.local>
2017-02-10 20:21:08 -0600

    Plans
    So I am planning on working with these vaibles to run if statements to
    check each of the 5 numbers to see if it is a +, -, or a 0

---
+++ HW2_32/src/hw2_32/HW2_32.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw2_32;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Noah
+ */
+public class HW2_32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please print out 5 digits. "
+                + "(They can be positive or negative)");
+        //identifying the varibles
+        int num1,
+                num2,
+                num3,
+                num4,
+                num5,
+                posnum,
+                negnum,
+                zero;
+        
+        
+        
+        
+        
+    }
+    
+}
