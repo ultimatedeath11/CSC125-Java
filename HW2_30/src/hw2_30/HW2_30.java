37a57d46b8de6f8c9ecb1ae0d457585ecd071299
Noah Franck <Noah@Noahs-iMac-3.local>
2017-02-10 19:40:36 -0600

    Homework ch 2.30
    assignment is to basically take 5 digits and separate them into their
    own variables to then display them separately.

---
+++ HW2_30/src/hw2_30/HW2_30.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw2_30;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Noah
+ * 
+ * Write an application that inputs one number consisting of 5 digits from the
+ * user, seporates the number into its individual digits and prints the digits 
+ * separated from one another by 3 spaces each
+ * 
+ */
+public class HW2_30 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Listing all of the varibles planned to use
+        int inputnum,
+                tenThous,
+                thousandths,
+                hundreths,
+                tens,    
+                ones;
+        
+    }
+    
+}
