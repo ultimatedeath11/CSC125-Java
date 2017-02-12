9a9d6a5a50902f6304d857d8c647c901e38b37b0
Noah Franck <ultimatedeath11@icloud.com>
2017-02-12 12:01:41 -0600

    continued testing
    I am continueing testing and working of the code for this project. It is
    still giving me trouble. I have tried changing the varible types to
    Float and to double. Both are still causing the

--- HW2_35/src/hw2_35/HW2_35.java
+++ HW2_35/src/hw2_35/HW2_35.java
@@ -9,61 +9,72 @@ import java.util.Scanner;
 
 /**
  *
- * @author Noah
- * 
+ * @author Noah Franck
+ *
  * This Application is going to be different, it involves lots of research
  * instead im going to use rough estamates.
- * 
+ *
  */
 public class HW2_35 {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
+
     Scanner input = new Scanner(System.in);
-    
-    //defining the new variables
-    
+
+    /*
+    defining the new variables
+    I use some as intagers and some as doubles
+    Some are doubles incase they use decimal points
+    */
+
     int Tmiles,
-            numOfPeopleinCar,
-            mperg;
-    
+        numOfPeopleinCar,
+        mperg;
+
     double CostGas,
-            parkFee,
-            savings,
-            tollsPerDay;
-    
+           parkFee,
+           tollsPerDay;
+
+     float savings,
+      initialsavings,
+      CostGas,
+      parkFee,
+     tollsPerDay;
+
     //prompting and storing total miles deiven each day
-    
+
     System.out.println("How many total people can fit in one car?");
     System.out.print(">");
     numOfPeopleinCar = input.nextInt();
-    
-    
+
+
     System.out.println("What is your total miles driven each day?");
     System.out.print(">");
     Tmiles = input.nextInt();
-    
+
     System.out.println("What is the cost of Gas in your city?");
     System.out.print(">");
-    CostGas = input.nextInt();
-    
+    CostGas = input.nextDouble();
+
     System.out.println("How many miles per gallon does your car get?");
     System.out.print(">");
     mperg = input.nextInt();
-    
+
     System.out.println("What is your daily parking fee?");
     System.out.print(">");
-    parkFee = input.nextInt(); 
-    
+    parkFee = input.nextDouble();
+
     System.out.println("How many tolls do you pass each day?");
     System.out.print(">");
-    tollsPerDay = input.nextInt();
-    
-    savings = (Tmiles * (mperg / CostGas) * numOfPeopleinCar + (tollsPerDay * numOfPeopleinCar * parkFee));
-    
+    tollsPerDay = input.nextDouble();
+
+    initialsavings = (Tmiles * mperg * CostGas + parkFee + tollsPerDay);
+
+    savings = (numOfPeopleinCar * initialsavings);
+
         System.out.printf("The total savings that can be saved is %d", savings);
     }
-    
+
 }
