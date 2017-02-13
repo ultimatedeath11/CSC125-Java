3266e6c8198912b8569b2e32d2bb279729f8adba
Noah Franck <ultimatedeath11@icloud.com>
2017-02-13 14:25:21 -0600

    Practicing creating new classes
    I have practiced creating classes in these files,  from my textbook.
    
    The idea behind this is to create a small program that will write people
    a bank account and will hold it and can then output it. As the textbook
    goes through ch. 3 more will be added to this.
    
    Soon to push the reamaining files up.

---
+++ CSC125_ch3/src/Account.java
@@ -0,0 +1,28 @@
+/*
+ * This is from my textbook working on practice problems
+ * these came stright form the book tho help me with learning the code
+ * 
+ */
+
+/**
+ *
+ * @author noah
+ */
+public class Account { //the class declaration
+    private String name; //instance barible 
+    
+    //method to set the name in the object
+    public void setName(String name)
+    {
+        this.name = name; //store the name
+        
+    }
+    
+    //method to retrieve the name from the object
+    public String getName()
+    {
+        return name; //return value of name to caller 
+      
+    }
+    
+}
