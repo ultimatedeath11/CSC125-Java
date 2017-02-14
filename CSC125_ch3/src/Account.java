8de2ddc55d1f1aeebcada22f006a3393662a654b
Noah Franck <ultimatedeath11@icloud.com>
2017-02-14 15:25:32 -0600

    added stuff to comments
    comments added on change to code

--- CSC125_ch3/src/Account.java
+++ CSC125_ch3/src/Account.java
@@ -1,7 +1,7 @@
 /*
  * This is from my textbook working on practice problems
  * these came stright form the book tho help me with learning the code
- * 
+ *
  */
 
 /**
@@ -9,34 +9,46 @@
  * @author noah
  */
 public class Account { //the class declaration
-    private String name; //instance barible 
-    private double balance; 
-    
+    private String name; //instance barible
+    private double balance;
+
     //constructor name initilizeds name with paramert name... ID
     public Account(String name) //constructio name is class name
     {
         this.name = name;
-        
-        //validate that the balance is greater than 0.0; if it's noe, 
+
+        //validate that the balance is greater than 0.0; if it's noe,
         //instance variable balance keeps its default initial vale of 0.0
         if(balance > 0.0)
             this.balance = balance;
-        
     }
-    
-    
+
+    //creating deposit
+    public void deposit(double depositAmount)
+    {
+         if(depositAmount > 0.0) //makeing sure it is valad
+          balance = balance + depositAmount;
+    }
+
+//method that returns the account balance
+     public double getBalance()
+     {
+          return balance;
+     }
+
+
     //method to set the name in the object
     public void setName(String name)
     {
         this.name = name; //store the name
-        
+
     }
-    
+
     //method to retrieve the name from the object
     public String getName()
     {
-        return name; //return value of name to caller 
-      
+        return name; //return value of name to caller
+
     }
-    
+
 }
