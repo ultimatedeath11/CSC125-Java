4da00b1bbaccb0a0a6766bfb8f52ac25a86f5863
Noah Franck <ultimatedeath11@icloud.com>
2017-02-14 16:20:57 -0600

    D-bugged
    I debugged stuff:
    got rid of duplicate lines
    can't create blank account bc there defiend in the program
    can't print names of the input when already defined.

--- CSC125_ch3/src/AccountTest.java
+++ CSC125_ch3/src/AccountTest.java
@@ -24,20 +24,19 @@ public class AccountTest {
         Account account2 = new Account("John Blue");
         
 
-        Account account1 = new Account("Jane Green", 50.00);
-        Account account2 = new Account("John Blue", -7.53);
+     
         //create an Account object and assign it to myAccount
         
-        Account myAccount = new Account(); //this will take the program stright
+     
         // to the 11th line of Account.java file
         //display initial value of name (Null)
-        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+      
 
         
         //displays initial value of name for each account
         
         System.out.printf("account1 name is: %s%n", account1.getName());
-        System.out.printf("account2 name is: %s%n", account.getName());
+        System.out.printf("account2 name is: %s%n", account2.getName());
     }
     
 }
