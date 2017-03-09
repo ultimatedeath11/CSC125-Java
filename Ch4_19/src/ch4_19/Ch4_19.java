1624013215dc70b170a8b4aabf8336516f994a64
Noah Franck <ultimatedeath11@icloud.com>
2017-03-09 00:23:41 -0600

    Commented the assignment
    I added the assignment into the comments and are pretty simple and
    explanitr

--- Ch4_19/src/ch4_19/Ch4_19.java
+++ Ch4_19/src/ch4_19/Ch4_19.java
@@ -15,6 +15,27 @@ public class Ch4_19
     /**
      * @param args the command line arguments
      */
+    
+    /**
+     * large company pays its salespeopel on a commission basis.
+     * the sales people recieve $200 a week plus 9% of their 
+     * gross sales for that week!
+     * 
+     * item values are as followed:
+     * 
+     *239.99
+     *129.75
+     * 99.95
+     * 350.89
+     * 
+     * Develop an app that will input what someone sales for last week and 
+     * calculates what he earned 
+     * 
+     * note that there is no limit to amount that can be sold. 
+     * 
+     * @param args 
+     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
     }
