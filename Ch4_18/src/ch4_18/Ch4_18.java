235f611dfea50bd67a847fc8aa9cb5d0ae7d80f8
Noah Franck <ultimatedeath11@icloud.com>
2017-03-09 00:13:26 -0600

    Litterally just wrote the assignment
    Little confused by the instructions but that is what the textbook gave
    me, so I am going to do my best to wrie it.

--- Ch4_18/src/ch4_18/Ch4_18.java
+++ Ch4_18/src/ch4_18/Ch4_18.java
@@ -15,8 +15,23 @@ public class Ch4_18
     /**
      * @param args the command line arguments
      */
+    
+    /**
+     * Assignment is credit limit calculator, develop a java app 
+     * that determines wheather any of severy department sore customers 
+     * have exceeded the credit limit on a charge account. 
+     * for each customer the following are available:
+     * 
+     * account number
+     * balance at begining of month
+     * total of all items charged by customer this month
+     * total of all credits applied ot the customer's account this month
+     * allowed credit limit
+                    
+    
+    **/
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
