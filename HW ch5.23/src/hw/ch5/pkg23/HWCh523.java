730331613a801b147e89f980919a830ab023af20
Noah Franck <Noah@Noahs-iMac-3.local>
2017-03-17 17:45:16 -0500

    The Problem
    I have stated what the problem is for this exercise in the homework.

--- HW ch5.23/src/hw/ch5/pkg23/HWCh523.java
+++ HW ch5.23/src/hw/ch5/pkg23/HWCh523.java
@@ -14,8 +14,19 @@ public class HWCh523 {
     /**
      * @param args the command line arguments
      */
+    
+    /**
+     * Use DeMorgan's laws to write equivalent expression for the following, and
+     * the new expression in each case produce the same value:
+     * 1. !(x < 5) && !(y >= 7)
+     * 2. !(a==b)|| !(g != 5)
+     * 3. !((x <=8) && (y > 4))
+     * 4. !((i > 4) || (j<=6))
+     * @param args 
+     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
