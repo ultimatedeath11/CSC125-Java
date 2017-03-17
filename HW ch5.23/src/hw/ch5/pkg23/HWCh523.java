21e76d35e70ec74fa3a60cdc78d4a2e91e1411cb
Noah Franck <Noah@Noahs-iMac-3.local>
2017-03-17 18:26:18 -0500

    Testing the sets
    Pretty simple just make two statements that are the same but are stated
    in different formats.

--- HW ch5.23/src/hw/ch5/pkg23/HWCh523.java
+++ HW ch5.23/src/hw/ch5/pkg23/HWCh523.java
@@ -19,7 +19,7 @@ public class HWCh523 {
      * Use DeMorgan's laws to write equivalent expression for the following, and
      * the new expression in each case produce the same value:
      * 1. !(x < 5) && !(y >= 7)
-     * 2. !(a==b)|| !(g != 5)
+     * 2. !(a==b) || !(g != 5)
      * 3. !((x <=8) && (y > 4))
      * 4. !((i > 4) || (j<=6))
      * @param args 
@@ -27,6 +27,50 @@ public class HWCh523 {
     
     public static void main(String[] args) {
         
+        /**
+         * Opposites:
+         * 1. !((x < 5) || (y >=7))
+         * 2. !((a==b) && (g!=5))
+         * 3. ( !(x <= 8) || !(y>4))
+         * 4. (!(i>4) && (j<=6))
+         */
+        
+        int x = 4,
+                y = 8,
+                a = 9,
+                b = 9,
+                i = 3,
+                g = 5,
+                j = 7;
+                
+        
+        if(!((x < 5) && !(y >= 7))){
+            System.out.println("1 is true");
+        }
+        if ((!(x < 5) || (y >=7))){
+            System.out.println("1 is fully true");
+        }
+        
+        if(!(a==b) || !(g != 5)){
+            System.out.println("2 is true");
+        }
+        if(!((a==b) && (g!=5))){
+            System.out.println("2 is fully true");
+        }
+        
+        if(!((x <=8) && (y > 4))){
+            System.out.println("3 is true");
+        }
+        if(( !(x <= 8) || !(y>4))){
+            System.out.println("3 is fully true");
+        }
+        
+        if(!((i > 4) || (j<=6))){
+            System.out.println("4 is true");
+        }
+        if((!(i>4) && !(j<=6))){
+            System.out.println("4 is fully full");
+        }
     }
     
 }
