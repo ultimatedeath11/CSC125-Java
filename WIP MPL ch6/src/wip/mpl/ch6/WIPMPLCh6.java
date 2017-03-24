8c6734814b0ad3b49db9e7f21f16a39e21dcbacf
Noah Franck <ultimatedeath11@icloud.com>
2017-03-24 13:24:18 -0500

    created My programming lab ch 6 test enviornment

---
+++ WIP MPL ch6/src/wip/mpl/ch6/WIPMPLCh6.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wip.mpl.ch6;
+import java.util.
+/**
+ *
+ * @author NoahFranck
+ */
+public class WIPMPLCh6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
