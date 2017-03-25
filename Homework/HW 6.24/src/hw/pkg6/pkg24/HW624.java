3cd9a2a948c0401b467214e605ce37dd7aec4744
Noah Franck <ultimatedeath11@icloud.com>
2017-03-25 15:02:52 -0500

    finished this assignment
    Basically finished it in 15 mins wasn’t that had. just made a method
    that would test is something was considered a ‘perfect’ number and then
    return it if it was or not, if not.

--- Homework/HW 6.24/src/hw/pkg6/pkg24/HW624.java
+++ Homework/HW 6.24/src/hw/pkg6/pkg24/HW624.java
@@ -9,13 +9,54 @@ package hw.pkg6.pkg24;
  *
  * @author NoahFranck
  */
+
+//Write a method called isPerfect to compare if all numbers are perfect between 
+//1-1000
+
+
 public class HW624 {
 
     /**
      * @param args the command line arguments
      */
+    
+    public HW624(){
+    
+}
+    
+    public int isPerfect(int toTest){
+        
+        
+        int i = toTest;
+        int sum = 0;
+        while(i > 0){
+            if(toTest % i == 0){
+                sum = sum + i;
+            }
+            
+            i = i-1;
+        }
+        sum = sum - toTest;
+        if(toTest == sum){
+            System.out.println(toTest + " Is perfect ");
+            return toTest;
+        }else{
+            
+            return toTest;
+        }
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int testAll = 1;
+        
+        HW624 perfectTest = new HW624();
+        
+        
+        for(;testAll < 1000; testAll++){
+        perfectTest.isPerfect(testAll);}
+        
+        
     }
     
 }
