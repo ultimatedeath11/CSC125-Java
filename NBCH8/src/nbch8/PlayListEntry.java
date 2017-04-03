5b0764cbb682faf924071b2c4df6f8e9653cfd14
Noah Franck <ultimatedeath11@icloud.com>
2017-04-03 16:41:32 -0500

    created java class PlayListEntry
    This is the class PlayListEntry for the My programming lab. Right now
    all that is in the class is the instructions form the assignment.

---
+++ NBCH8/src/nbch8/PlayListEntry.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nbch8;
+
+/**
+ *
+ * @author NoahFranck
+ */
+
+/**
+ * Write the definition of a class  PlayListEntry containing:
+An instance variable  title of type  String , initialized  to the empty String.
+An instance variable  artist of type  String , initialized  to the empty String.
+An instance variable  playCount of type  int , initialized  to 0.
+In addition, your PlayList class  definition should provide an appropriately 
+* named  "get" method  and "set" method  for each of these. 
+No constructor  need be defined. 
+ * @author NoahFranck
+ */
+public class PlayListEntry {
+    
+}
