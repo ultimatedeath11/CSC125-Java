c4857be7b5a9fa012862835d9adb2b2246a0bcf6
Noah Franck <ultimatedeath11@icloud.com>
2017-04-03 11:41:20 -0500

    Class & constructor
    the class, I have added ‘extends object’ (java does this on its own,
    but stating it specifically here)
    
    I have also added a constructor and defined the varuiables inside of
    the class.
    
    Inside of the constructor it checks to make sure that it is valid ID
    with if statements.

--- Ch 9 inclass/src/ch/pkg9/inclass/Ch9Inclass.java
+++ Ch 9 inclass/src/ch/pkg9/inclass/Ch9Inclass.java
@@ -8,14 +8,34 @@ package ch.pkg9.inclass;
 /**
  *
  * @author NoahFranck
+ * from textbook pg. 366
  */
-public class Ch9Inclass {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Ch9Inclass extends Object{
+    private final String firstName;
+    private final String lastName;
+    private final String socialSecurityNumber;
+    private double grossSales;//gross weekly sales
+    private double commissionRate; //commission percentage
+    
+    public Ch9Inclass(String firtName, String lastName, String 
+            SocialSecurityNumber, double grossSales, double commissionRate){
+        //implicitely call to object's default customomer occurs here
+        
+        // if grossSales is invalad throw an exception
+        if(grossSales < 0.0)
+            throw new IllegalArgumentException("Gross Sales must be >= 0.0");
+        
+        //if commissionRate is invalid thow exception
+        if(CommissionRate <= 0.0 || comissionTate >= 1.0)
+            throw new IllegalArgumentException("Commission rate must be > 0.0 "
+                    + "and < 1.0");
+        
+        
+    }
+    
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        //working on extending classes
     }
     
 }
