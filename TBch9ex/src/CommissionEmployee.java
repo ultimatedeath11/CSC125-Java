c0da1eba98994d5ebbe071df155b5862c74968ba
Noah Franck <ultimatedeath11@icloud.com>
2017-04-24 14:26:08 -0500

    First part of assignement
    So I have written the first 5 variables, in private and final, and i
    have extended the Object, and I created the constructor  with if
    statements to double check its validity.

--- TBch9ex/src/CommissionEmployee.java
+++ TBch9ex/src/CommissionEmployee.java
@@ -8,6 +8,35 @@
  *
  * @author NoahFranck
  */
-public class CommissionEmployee {
+public class CommissionEmployee extends Object {
+    private final String firstName;
+    private final String lastName;
+    private final String SocialSecurityNumber;
+    private double grossSales;
+    private double commissionRate;
+    
+    //fige-argument constructor
+    public CommissionEmployee(String firstName, String LastName, 
+            String socialSecurityNumber, double grossSales, 
+            double CommissionRate){
+        //implicit call to object's default constructor occurs here. 
+        
+        //if grossSales is invalid throw exception
+        if(grossSales < 0.0)
+            throw new IllegalArgumentException("Gross Sales Must be >= 0.0");
+        
+        //if CommissionRate is invalid throw the expression
+        if(commissionRate < 0.0 || commissionRate >= 1.0)
+            throw new IllegalArgumentException("Commission rate must be >= 0.0"
+            +"and < 1.0");
+        
+        
+        //setting everyhing = to everything else
+        this.firstName = firstname;
+        this.lastName = lastname;
+        this.SocialSecurityNumber = socialSecurityNumber;
+        this.commissionRate = CommissionRate;
+        this.grossSales = grossSales;
+    }
     
 }
