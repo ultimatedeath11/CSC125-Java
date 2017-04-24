45c1edf4a4257ed2d2f66f896420f43925330c55
Noah Franck <ultimatedeath11@icloud.com>
2017-04-24 15:26:13 -0500

    Created the CommissionEmployeeTest class
    Next class from the textbook.

---
+++ TBch9ex/src/CommissionEmployeeTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author NoahFranck
+ */
+public class CommissionEmployeeTest {
+    public static void main(String[] args){
+        //instantaine commissionEmployee object
+        CommissionEmployee employee = new commissionEmployee(
+        "Sue","Jones", "222-222-2222", 10000, .06);
+        
+        //get commission employee data
+        
+    }
+    
+}
