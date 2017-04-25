9dab8ab9a909824f3dbb35785be08119053e9b4a
Noah Franck <ultimatedeath11@icloud.com>
2017-04-25 10:41:54 -0500

    everything form the CommissionEmployee class
    Just copied and pasted the commission employee class into this class. I
    still have to make the changes for the base pay part though.

--- TBch9ex/src/BasePlusCommissionEmployee.java
+++ TBch9ex/src/BasePlusCommissionEmployee.java
@@ -9,5 +9,81 @@
  * @author NoahFranck
  */
 public class BasePlusCommissionEmployee {
+        private final String firstName;
+    private final String lastName;
+    private final String SocialSecurityNumber;
+    private double grossSales;
+    private double commissionRate;
+    
+    //fige-argument constructor
+    public CommissionEmployee(String firstName, String lastName, 
+            String socialSecurityNumber, double grossSales, 
+            double CommissionRate){
+        //implicit call to object's default constructor occurs here. 
+        
+        //if grossSales is invalid throw exception
+        if(grossSales < 0.0)
+            throw new IllegalArgumentException("Gross Sales Must be >= 0.0");
+        
+        //if CommissionRate is invalid throw the expression
+        if(commissionRate < 0.0 || commissionRate >= 1.0)
+            throw new IllegalArgumentException("Commission rate must be >= 0.0"
+            +"and < 1.0");
+        
+        
+        //setting everyhing = to everything else
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.SocialSecurityNumber = socialSecurityNumber;
+        this.commissionRate = CommissionRate;
+        this.grossSales = grossSales;
+    }
+    
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    public String getLastName(){
+        return lastName;
+    }
+    
+    public String getSocialSecurityNumber(){
+        return SocialSecurityNumber;
+    }
+    
+    public void setGrossSales(double grossSales){
+        if (grossSales < 0.0)
+           throw new IllegalArgumentException("Gross sales must be > 0.0"); 
+        this.grossSales = grossSales; 
+    }
+    
+    public double getGrossSales(){
+        return grossSales;
+    }
+    
+    public void setCommissionRate(double commissionRate){
+        if(commissionRate < 0.0 || commissionRate > 1.0)
+            throw new IllegalArgumentException("Commission Rate has to be "
+                    + "between 0.0 & 1.0 ");
+    this.commissionRate = commissionRate;
+    }
+    
+    public double getCommisssionRate(){
+        return commissionRate;
+    }
+    
+    //calculating earnings
+    public double earnings(){
+        return commissionRate * grossSales;
+    }
+    
+    //returnn String representstation of CommissionEmployee object
+    @Override //indicates that this method overrides a superclass method
+    public String toString(){
+        return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f",
+                "Commissin employe", firstName, lastName, "Social Security"
+                        + " Number", SocialSecurityNumber, "gross sales",
+                        grossSales, "commission rate", commissionRate);
+    }
     
 }
