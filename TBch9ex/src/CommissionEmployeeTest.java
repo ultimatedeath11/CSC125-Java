47cf3475a8fbb82fd8b0c6ac491c5072adea89d4
Noah Franck <ultimatedeath11@icloud.com>
2017-04-25 10:39:39 -0500

    finished out the commission employee test class.
    Basically wrote everything out similar to how the book does.

--- TBch9ex/src/CommissionEmployeeTest.java
+++ TBch9ex/src/CommissionEmployeeTest.java
@@ -16,8 +16,17 @@ public class CommissionEmployeeTest {
         
         //get commission employee data
         System.out.println("Employee information obtained by get methods:");
-        System.out.printf("%n%s %s%n,", "First name is", employee.getFirstName());
+        System.out.printf("%n%s %s%n,", "First name is: ", employee.getFirstName());
+        System.out.printf("%n%s %s%n,", "Last name is: ", employee.getLastName());
+        System.out.printf("%n%s %s%n,", "Social Security number is ", employee.getSocialSecurityNumber());
+        System.out.printf("%n%s %s%n,", "Gross sales are ", employee.getGrossSales());
+        System.out.printf("%n%s %s%n,", "Commission Rate is: ", employee.getCommisssionRate());
         
+        employee.setGrossSales(5000);
+        employee.setCommissionRate(.1);
+        
+        System.out.printf("%n%s: %n%n%s%n", "Updated employee info from the"
+                + " tostring", employee);
     }
     
 }
