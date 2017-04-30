b693bc96970b66efcd196a91e2c39bc4f5d952ee
Noah Franck <ultimatedeath11@icloud.com>
2017-04-29 22:10:43 -0500

    Created DrawPanel class
    Created the draw Panel, still a good bit of stuff to do.

---
+++ Homework/CH4OGUI/src/DrawPanel.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * Drawing application that is able to draw stuff. I am testing. 
+ * 
+ * @author NoahFranck
+ */
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawPanel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
