33c337c2b3d31e7d8352f34ba1cdddd827ff1758
Noah Franck <ultimatedeath11@icloud.com>
2017-05-02 14:40:48 -0500

    Created the main class for the group project.
    Camron and I are working on this project and I wrote the rules and
    summery inside of the comment near the top.

---
+++ MatrixGroup Project/src/MainClass.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * Goal is to write an application that will execute a math formula. 
+ * Finding the Determinant for at minimum 2x2 matrix, with bonus of taking a 
+ * matrix at any size. The matrix's dimensions are to be passed from the 
+ * user. 
+ * 
+ * @author NoahFranck & Camron Johnson
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
