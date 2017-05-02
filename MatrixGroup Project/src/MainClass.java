0282e44b1c1e03024cb6eefe699dce6c388452f1
Noah Franck <ultimatedeath11@icloud.com>
2017-05-02 15:26:18 -0500

    created 3 arrays and comments

--- MatrixGroup Project/src/MainClass.java
+++ MatrixGroup Project/src/MainClass.java
@@ -13,6 +13,8 @@ import java.util.Scanner;
  * Finding the Determinant for at minimum 2x2 matrix, with bonus of taking a 
  * matrix at any size. The matrix's dimensions are to be passed from the 
  * user. 
+ * In addition the program should be able to handle multiplying 2 Matrixes 
+ * together and an integer by the matrix. 
  * 
  * @author NoahFranck & Camron Johnson
  */
@@ -22,11 +24,17 @@ public class MainClass {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
+        //below will be variables creation
         int SizeOfMatrix = 0; //variable for the dimensions of the matrix
+        double matrix1[][];//creating the first two dimensional array
+        double matrix2[][];//creating the second 2 dimenisonal array
+        double Solution[]; //array for solutions to be printed. 
+        //maybe redundant based on wheather I use methods or not. 
         
         //below will ask the user and then will pass their answer into the var
         System.out.println("Please enter the dimensions of the Matrix");
         SizeOfMatrix = input.nextInt();
+        
         
         
         
