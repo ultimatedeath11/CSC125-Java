1b696c5fbd2d887afe6fce2027524696e46eb559
Noah Franck <ultimatedeath11@icloud.com>
2017-05-02 14:46:11 -0500

    Added the Scanner method and fixed formatting.

--- MatrixGroup Project/src/MainClass.java
+++ MatrixGroup Project/src/MainClass.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,11 +18,13 @@
  */
 public class MainClass {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        
+        
+        
     }
     
 }
