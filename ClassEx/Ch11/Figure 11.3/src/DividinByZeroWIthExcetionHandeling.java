a563cab7a8d3942171627a495cc7aaa64d513ce1
Noah Franck <ultimatedeath11@icloud.com>
2017-05-09 17:00:48 -0500

    Lot of changes, but finished
    lns16-21{
    Created a method quotient that will accept two numbers and divide by
    them.
    
    this is where our error is
    }
    
    ln28-48{
    
    Big loop that will continue so long as the bollean variable is true,
    and that is only set to true if the try scenario does not return an
    error.
    
    ln39-43{
    
    This is the code that will try something else if there is an error in
    the code form the return statement.
    
    }
    
    }

--- ClassEx/Ch11/Figure 11.3/src/DividinByZeroWIthExcetionHandeling.java
+++ ClassEx/Ch11/Figure 11.3/src/DividinByZeroWIthExcetionHandeling.java
@@ -1,4 +1,5 @@
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
@@ -12,14 +13,40 @@ import java.util.Scanner;
  * @author NoahFranck
  */
 public class DividinByZeroWIthExcetionHandeling {
-
+//demonstrates throwing an exception when '/ by 0' occurs. 
+    public static int quotient(int n, int d) throws ArithmeticException {
+        return n/d;
+    }//possible division by zero
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner Scanner = new Scanner(System.in);
-        
-        
+        boolean continueLoop = true; //determins if more input is needed
+        do{
+            try{ //read 2 numbers and calculate quoient
+                System.out.print("Enter a Numerator: ");
+                int numer = Scanner.nextInt();
+                System.out.print("please enter a Denominator");
+                int denom = Scanner.nextInt();
+                
+                int result = quotient(numer, denom);
+                System.out.printf("%nResult: %d / %d = %d%n",numer, denom,
+                        result);
+                continueLoop = false; //input Sucessful; end looping process. 
+            }catch (InputMismatchException inputMismatchExcetption){
+                System.err.printf("%nException: %s%n", inputMismatchException);
+                Scanner.nextLine();//discard input so user can input another
+                System.out.printf("You must enter integers. Please try again. "
+                        + "%n%n");   
+            }catch(ArithmeticException arithmeticException){
+                System.err.printf("%nException:%s%n", arithmeticException);
+                System.out.printf("Zero is an invalid Denominator. Please try"
+                        + "again %n%n");
+            }
+        }while(continueLoop);
         
     }
     
