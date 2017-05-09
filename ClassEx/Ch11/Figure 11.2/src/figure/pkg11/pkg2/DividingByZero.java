efd82021591a24e3d6eba4e4e283d8e25645ef3f
Noah Franck <ultimatedeath11@icloud.com>
2017-05-09 16:12:15 -0500

    Created the Main class
    The purpose of the class is to divide by 0 with exception handling.

---
+++ ClassEx/Ch11/Figure 11.2/src/figure/pkg11/pkg2/DividingByZero.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package figure.pkg11.pkg2;
+
+/**
+ *
+ * @author NoahFranck
+ */
+public class DividingByZero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+}
