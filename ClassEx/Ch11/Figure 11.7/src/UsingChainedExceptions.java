f33f08f0900060c54775a208908d5b6772eb943e
Noah Franck <ultimatedeath11@icloud.com>
2017-05-09 23:27:04 -0500

    Finished the using Chained expressin
    Exceptions passed through 3 different methods.

--- ClassEx/Ch11/Figure 11.7/src/UsingChainedExceptions.java
+++ ClassEx/Ch11/Figure 11.7/src/UsingChainedExceptions.java
@@ -15,6 +15,32 @@ public class UsingChainedExceptions {
      */
     public static void main(String[] args) {
         
+        try{
+            method1();
+        }
+        catch(Exception exception){
+            exception.printStackTrace();
+        }
+    }
+    
+    public static void method1() throws Exception{
+        try {
+            method2();
+        }catch(Exception exception){
+            throw new Exception("Exception thrown in method1", exception);
+        }
+    }
+    
+    public static void method2() throws Exception{
+        try {
+            method3();
+        }catch(Exception exception){
+            throw new Exception("Exception thrown in method2", exception);
+        }
+    }
+    
+    public static void method3() throws Exception{
+        throw new Exception("Exception thrown in method3");
     }
     
 }
