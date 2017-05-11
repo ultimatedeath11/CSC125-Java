2f0667ae8d5b1c5d535f5c036a32ec5ae3d4264d
Noah Franck <ultimatedeath11@icloud.com>
2017-05-10 23:06:15 -0500

    First bit of code
    SO here we have built a string an an array of characters… Printing the
    length of the string that is hard coded into it.
    
     then looping through the character at to display the character in use,
    but reversed.
    
    Lines 32-34 confuse me a bit, but I will work on them here in a bit.

--- ClassEx/Ch14/Figure 14.2/src/StringMiscellaneous.java
+++ ClassEx/Ch14/Figure 14.2/src/StringMiscellaneous.java
@@ -15,8 +15,28 @@ public class StringMiscellaneous {
      */
     public static void main(String[] args) {
         
+        String s1 = "hello there";
+        char[] charArray = new char[5];
         
+        System.out.printf("s1: %s", s1);
         
+        //test length method
+        System.out.printf("%nLength of s1: %d", s1.length());
+        
+        //loop through characters in s1 with charAt and display reversed
+        System.out.printf("%n The string reversed is: ");
+        for(int count = s1.length() - 1; count >= 0; count--){
+            System.out.printf("%c ", s1.charAt(count));
+        }
+        
+        //copy characters from string into charArray
+        s1.getChars(0, 5, charArray, 0);
+        System.out.printf("%nThe CHaracter array is: ");
+        
+        for(char character : charArray)
+            System.out.print(character);
+        
+        System.out.println();
     }
     
 }
