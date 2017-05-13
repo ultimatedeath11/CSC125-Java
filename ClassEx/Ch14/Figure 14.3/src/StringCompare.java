dcf29b0978736d460e1a7abe71571257a9c9c452
Noah Franck <ultimatedeath11@icloud.com>
2017-05-13 18:06:38 -0500

    filled in the rest of the test comparison

--- ClassEx/Ch14/Figure 14.3/src/StringCompare.java
+++ ClassEx/Ch14/Figure 14.3/src/StringCompare.java
@@ -23,10 +23,25 @@ public class StringCompare {
         "s1 = %s%ns2 = %s%ns3 = %s%ns4 = %s%n%n", s1, s2, s3, s4);
         
         //testing for equality
-        if(s1.equals("hello"))
+        if(s1.equals("hello"))//true
             System.out.println("S1 equals \"hello\"");
         else
             System.out.println("s1 does not equal \"hello\"");
+        
+        //testing for equality for ==
+        if(s1 == "hello") //false they are not the same object
+            System.out.println("s1 is the same object as \"hello\"");
+        else 
+            System.out.println("S1 is not the same object as \"Hello\"");
+        
+        //test for equality (Ignore case)
+        if(s3.equalsIgnoreCase(s4))
+            System.out.printf("s3 equals %s with case ignored%n", s3, s4);
+        else 
+            System.out.println("s3 does not equal s4");
+        
+        
+        
     }
     
 }
