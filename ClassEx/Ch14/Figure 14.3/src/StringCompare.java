6df269b96e95e02b9893dbd322f02cd113e42417
Noah Franck <ultimatedeath11@icloud.com>
2017-05-13 17:53:54 -0500

    first bit added
    I created the 4 Strings.
    printed them all out, and then it tests whether they are equal or not.

--- ClassEx/Ch14/Figure 14.3/src/StringCompare.java
+++ ClassEx/Ch14/Figure 14.3/src/StringCompare.java
@@ -14,7 +14,19 @@ public class StringCompare {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String s1 = new String("hello");//copy of s1
+        String s2 = "goodbye";
+        String s3 = "Happy Birthday";
+        String s4 = "happy birthday";
+        
+        System.out.printf(
+        "s1 = %s%ns2 = %s%ns3 = %s%ns4 = %s%n%n", s1, s2, s3, s4);
+        
+        //testing for equality
+        if(s1.equals("hello"))
+            System.out.println("S1 equals \"hello\"");
+        else
+            System.out.println("s1 does not equal \"hello\"");
     }
     
 }
