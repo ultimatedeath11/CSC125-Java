652d55415f60999e57b83f7227da380fb6d2f477
Noah Franck <ultimatedeath11@icloud.com>
2017-05-13 20:16:37 -0500

    finished the program

--- ClassEx/Ch14/Figure14.4/src/StringStartEnd.java
+++ ClassEx/Ch14/Figure14.4/src/StringStartEnd.java
@@ -15,6 +15,34 @@ public class StringStartEnd {
      */
     public static void main(String[] args) {
 
+        String[] strings = {
+            "started", 
+            "starting", 
+            "ended", 
+            "ending",
+        };
+        
+        //test method startsWith
+        for(String string : strings){
+            if(string.startsWith("st"))
+                System.out.printf("\"%s\" starts with \"st\"%n", string);
+        }
+        
+        System.out.println();
+        
+        //test metod startsWith starting form positon 2 of string
+        for(String string : strings){
+            if(string.startsWith("st"))
+                System.out.printf("\"%s\" starts with \"art\" at position 2%n",
+                        string);
+        }
+        System.out.println();
+        
+        //test method endsWith
+        for(String string : strings){
+            if(string.endsWith("ed"))
+                System.out.printf("\"%s\" ends with \"ed\"%n", string);
+        }
         
         
     }
