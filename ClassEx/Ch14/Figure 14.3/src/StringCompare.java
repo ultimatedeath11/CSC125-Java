00380923ee52603ca38c538636999a617f9db036
Noah Franck <ultimatedeath11@icloud.com>
2017-05-13 19:11:03 -0500

    finished the code for figure 14.2
    This is everything, and it compares all of the strings. And the strings
    are compared all in different ways.

--- ClassEx/Ch14/Figure 14.3/src/StringCompare.java
+++ ClassEx/Ch14/Figure 14.3/src/StringCompare.java
@@ -40,7 +40,29 @@ public class StringCompare {
         else 
             System.out.println("s3 does not equal s4");
         
+        //test comparedto
+        System.out.printf("%ns1.compareTo(s2) is %d", s1.compareTo(s2));
         
+        System.out.printf("%ns2.compareTo(s1) is %d", s2.compareTo(s1));
+        
+        System.out.printf("%ns1.compareTo(s1) is %d", s1.compareTo(s1));
+        
+        System.out.printf("%ns3.compareTo(s4) is %d", s3.compareTo(s4));
+        
+        System.out.printf("%ns4.compareTo(s3) is %d%n%n", s4.compareTo(s3));
+        
+        //test regionMatches (case sensative)
+        if(s3.regionMatches(0, s4, 0, 5))
+            System.out.println("First 5 characters of s3 and s4 match");
+        else
+            System.out.println("First 5 character of s3 adn s4 do not match");
+        
+        //test regionMatches (ignore case)
+        if(s3.regionMatches(true, 0, s4, 0, 5))
+            System.out.println("First 5 characters of s3 and s4 match with case ignored");
+        else
+            System.out.println("First 5 characters of s3 and s4 do not match");
+            
         
     }
     
